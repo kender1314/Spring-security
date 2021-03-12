120ea9e3e3a1a8258a496812097999c651e71cef
hejiang <18715770814@163.com>
2021-03-12 10:50:10 +0800

    自定义filter

--- 01-hello/src/main/java/com/example/demo/configure/SecurityConfigurationBySql.java
+++ 01-hello/src/main/java/com/example/demo/configure/SecurityConfigurationBySql.java
@@ -5,9 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.security.web.csrf.CsrfFilter;
 
 /**
  * 自定义编写实现类（继承UsernamePasswordAuthenticationFilter类，可以通过数据库进行认证）
@@ -17,6 +21,18 @@ public class SecurityConfigurationBySql extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private MyUserDetailsService userDetailsService;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+        web.ignoring()
+                .antMatchers("/demo/**", "/dasd/**");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(new MyFilter(), FilterSecurityInterceptor.class);
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
